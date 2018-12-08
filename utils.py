cc71139517fe81eb53590a8e741758d739528390
colbychance <duncanchance11@gmail.com>
2018-12-08 00:57:41 -0500

    handles 404 errors, can serve different file types

--- utils.py
+++ utils.py
@@ -1,16 +1,30 @@
 import sys
-FILE_NAME = 'index.html'
+
 def construct_header(body_size):
     return "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {}\n".format(body_size)
 
-def create_message():
-    body = open_file(FILE_NAME)
-    header = construct_header(len(body))
-    return header + "\n" + body
+def create_message(file):
+    try:
+        body = open_file(file)
+    except IOError:
+        body = "HTTP/1.1 404 Not Found".encode()
+    #have to add another new line before body, must encode when sending
+    #body is already encoded
+    header = construct_header(len(body)) + "\n"
+    return header.encode() + body
 
+#open file in binary for all file types
 def open_file(file_name):
-    f = open(file_name, 'r')
-    msg = ''
-    with open(file_name) as f:
+    with open(file_name, "rb") as f:
         msg = f.read()
     return msg
+
+def create_text_message(text):
+    return "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: {}\n\n{}".format(len(text), text)
+
+def parse_http(header):
+    line = header.split("\r\n")[0]
+    line = line.replace("GET /", "", 1)
+    line = line.replace(" HTTP/1.1", "", 1)
+    return line
+
--- webserver.py
+++ webserver.py
@@ -3,6 +3,7 @@ from threading import Thread, Lock
 import utils
 import http.client
 import sys
+import time
 
 if len(sys.argv) != 2:
         print('Invalid arguments. Must enter the port number to run on.')
@@ -11,7 +12,6 @@ try:
 except ValueError:
         print("Port number must be an integer")
         exit(1)
-# HOST = socket.gethostbyname(socket.gethostname())
 HOST = socket.gethostname()
 MAX_CONNECTIONS = 10
 s = None
@@ -28,14 +28,16 @@ def listening():
         a_thread = Thread(target=handle_client, args=(connection, addr), daemon=True)
         a_thread.start()
 
-
 def handle_client(connection, addr):
         print('===================New Client=================')
         while True:
                 data = connection.recv(2000)
                 print('=====Request Received=====')
                 print(data.decode())
-                connection.send(utils.create_message().encode())
+                requested_file = utils.parse_http(data.decode())
+                if len(requested_file) == 0:
+                        requested_file = "index.html"
+                connection.send(utils.create_message(requested_file))
         connection.close()
 
 def start():
